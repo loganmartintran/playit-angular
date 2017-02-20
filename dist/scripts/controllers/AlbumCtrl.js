a4d5d06fcce708011f65bf139a549a40db85bacc
Logan Tran <loganmartintran@icloud.com>
2017-02-20 12:20:39 -0600

    untracking /dist

--- dist/scripts/controllers/AlbumCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    function AlbumCtrl() {
-        this.albumData = albumPicasso;
-    }
-    
-    angular
-        .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl)
-})();
--- dist/scripts/controllers/LandingCtrl.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    function LandingCtrl() {
-        this.heroTitle ="Turn the Music Up!";
-    }
-    
-    angular
-        .module('blocJams')
-        .controller('LandingCtrl', LandingCtrl);
-})();
