74c8d8dd0bfd1f6ec5c6b54d51e07680b787790a
Logan Tran <loganmartintran@icloud.com>
2017-02-23 10:32:22 -0600

    completed cp7 assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -31,11 +31,21 @@
             currentSong = song;
         };
         
+        /**
+        *@function playSong
+        *@desc Plays currently playing song
+        *param {Object} song
+        */
+        
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
