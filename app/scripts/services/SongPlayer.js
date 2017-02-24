19acda221a844d8edf9c3bb4be4faae36349b4e9
Logan Tran <loganmartintran@icloud.com>
2017-02-24 12:44:43 -0600

    cp8 assignment with song name and total duration not showing on player bar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -23,8 +23,7 @@
         
         var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
-                SongPlayer.playing=null;
+                stopSong(song);
             }
             
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -38,12 +37,23 @@
         /**
         *@function playSong
         *@desc Plays currently playing song
-        *param {Object} song
+        *@param {Object} song
         */
         
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
+        }
+        
+        /*
+        *@function stopSong
+        *@desc stops the currently playing song
+        *@param {Object} song
+        */
+        
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
         }
         
         /**
@@ -104,8 +114,25 @@
             currentSongIndex--;
             
             if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(song);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        /*
+        *@function next
+        *@desc goes to the next track of the song that is playing
+        */
+        
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if (currentSongIndex > 5) {
+                stopSong(song);
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
